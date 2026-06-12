7e44640985334a8ff31861bb8c164db51fd99d29
blumouse <50953717+blumouse@users.noreply.github.com>
2026-06-13 00:34:41 +0900

    fix: 부모 세팅 로직 더더욱 강화

--- DDANZIT/impl/Framework/Component/Transform.cpp
+++ DDANZIT/impl/Framework/Component/Transform.cpp
@@ -269,6 +269,9 @@ void Transform::SetParent(Transform* parent)
 	{
 		_gameObject->_scene->hierarchy.pRootGameObjectList.push_back(_gameObject);
 
+		if (_parent != HIERARCY_ROOT)
+			_parent->RemoveChild(this);
+
 		_parent = parent;
 		return;
 	}
@@ -285,8 +288,7 @@ void Transform::SetParent(Transform* parent)
 			_gameObject->ownerHierarchy->pRootGameObjectList.begin(),
 			_gameObject->ownerHierarchy->pRootGameObjectList.end(), _gameObject),
 			_gameObject->ownerHierarchy->pRootGameObjectList.end());
-
-	if (_parent != HIERARCY_ROOT)
+	else /*(_parent != HIERARCY_ROOT)*/
 		_parent->RemoveChild(this);
 
 	parent->AddChild(this);
